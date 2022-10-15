6f9245a4dfae3de16cf17a7efe5b9bb2c21f7690
sevalnuroncu <sevalnuroncu@gmail.com>
2022-10-15 07:35:04 +0300

    inheritance example

---
+++ Challenge6/src/Homework6.java
@@ -0,0 +1,13 @@
+
+public class Homework6 {
+
+	public static void main(String[] args) {
+		Muzisyen muzisyen=new Muzisyen();
+		Piyanist piyanist=new Piyanist();
+		Solist solist=new Solist();
+		
+		
+
+	}
+
+}
---
+++ Challenge6/src/Muzisyen.java
@@ -0,0 +1,16 @@
+
+public class Muzisyen {
+	private String muzisyenAdi;
+	private int yas;
+	
+	public void konser() {
+		System.out.println("Konser ver");
+	}
+	public String getMuzisyenAdi() {
+		return muzisyenAdi;
+	}
+	public int getYas() {
+		return yas;
+	}
+	
+}
