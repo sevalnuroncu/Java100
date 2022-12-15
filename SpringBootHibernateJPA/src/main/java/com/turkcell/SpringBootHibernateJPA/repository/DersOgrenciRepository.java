ecc8059c72c8baee1d8e24e11e6b65afba741951
sevalnuroncu <sevalnuroncu@gmail.com>
2022-12-15 22:14:54 +0300

    jpaproject

--- SpringBootHibernateJPA/src/main/java/com/turkcell/SpringBootHibernateJPA/repository/DersOgrenciRepository.java
+++
@@ -1,9 +0,0 @@
-package com.turkcell.SpringBootHibernateJPA.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.turkcell.SpringBootHibernateJPA.model.DersOgrenci;
-
-public interface DersOgrenciRepository extends JpaRepository<DersOgrenci, Long> {
-
-}
--- SpringBootHibernateJPA/src/main/java/com/turkcell/SpringBootHibernateJPA/repository/OgrenciRepository.java
+++
@@ -1,9 +0,0 @@
-package com.turkcell.SpringBootHibernateJPA.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.turkcell.SpringBootHibernateJPA.model.Ogrenci;
-
-public interface OgrenciRepository extends JpaRepository<Ogrenci, Long> {
-
-}
